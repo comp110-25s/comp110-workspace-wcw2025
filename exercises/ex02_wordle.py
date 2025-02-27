6d0f845adbc034e7407aec3a32655b1513248cb4
wcw2025 <wcash@unc.edu>
2025-02-27 13:52:31 -0500

    wordletes

--- exercises/ex02_wordle.py
+++ exercises/ex02_wordle.py
@@ -3,5 +3,12 @@
 __author__: str = "730481634"
 
 
-def contains_char(word: str, letter: str)
-    return 10
+def contains_char(word: str, letter: str) -> bool:
+    """Searching a word for a certain letter"""
+    assert len(letter) == 1, f"len('{letter}') is not 1"
+    index: int = 0
+    while index <= len(word) - 1:
+        if letter == word[index]:
+            return True
+        index = index + 1
+    return False
