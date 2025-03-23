2694482ef1cee45f5c0f1ab8b1b84163ba64a3cf
wcw2025 <wcash@unc.edu>
2025-03-23 16:03:05 -0400

    invert_complete

--- exercises/ex03/dictionary.py
+++ exercises/ex03/dictionary.py
@@ -3,8 +3,17 @@
 __author__: str = "730481634"
 
 def invert(dictionary: dict[str,str]) -> dict[str,str]:
-    index: int=0
+    """Function inverting a given dictionary's keys and values"""
     inversion: dict[str,str] ={}
     for key in dictionary:
-        print(dictionary[key])
-    return dictionary
+        if dictionary[key] in inversion:
+            raise KeyError("You can't have a value more than once!")
+        else:
+            inversion[dictionary[key]]=key
+    return inversion
+
+def count(given_list: list[str]) -> int:
+    index: int = 0
+    while index<len(given_list):
+        return 5
+    return 5
