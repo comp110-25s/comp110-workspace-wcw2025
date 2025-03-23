ba37cd38f75ac882616aa13b5bb47968265ebd87
wcw2025 <wcash@unc.edu>
2025-03-23 15:14:20 -0400

    dictionary_work

---
+++ exercises/ex03/dictionary.py
@@ -0,0 +1,10 @@
+"""DOCSTRING"""
+
+__author__: str = "730481634"
+
+def invert(dictionary: dict[str,str]) -> dict[str,str]:
+    index: int=0
+    inversion: dict[str,str] ={}
+    for key in dictionary:
+        print(dictionary[key])
+    return dictionary
