6a84be30636ae64fa3a7dafe3963a459e7cb2436
wcw2025 <wcash@unc.edu>
2025-04-11 11:10:28 -0400

    in_class_work

---
+++ exercises/4-11.py
@@ -0,0 +1,24 @@
+from __future__ import annotations
+
+class Node:
+    value: int
+    next: Node | None
+
+    def __init__(self, value: int, next: Node | None)
+        self.value = value
+        self.next = next
+    
+    def __str__(self) -> str:
+        if self.next is None:
+            return f"{self.value} -> None"
+        else:
+            return f"{self.value} -> {self.next}"
+
+courses: Node = Node(110, Node(210, None))
+
+def last(head: Node) -> str:
+    """Function to return value of last node in linked list."""
+    if head.next is None: #Base case
+        return f"my value is {head.value}!"
+    else:
+        return last(head.next) #Recursive case
